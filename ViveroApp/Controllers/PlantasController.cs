f9a36d97ffae84dbf9ed36e117e97257144ae157
Jorge Luis <jorgedrluisj4@gmail.com>
2025-10-20 22:50:59 -0700

    Conexion a la base de datos

---
+++ ViveroApp/Controllers/PlantasController.cs
@@ -0,0 +1,25 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using ViveroApp.Models;
+
+namespace ViveroApp.Controllers
+{
+    public class PlantasController : Controller
+    {
+        private readonly string connectionString;
+
+        public PlantasController(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+        public async Task<IActionResult> Index()
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var plantas = await connection.QueryAsync<Plantas>("SELECT * FROM planta");
+                return View(plantas);
+            }
+        }
+    }
+}
---
+++ ViveroApp/Models/Plantas.cs
@@ -0,0 +1,8 @@
+
+namespace ViveroApp.Models
+{
+    public class Plantas{
+        public int Id {get; set;}
+        public string Nombre{get; set;}
+    }
+}
